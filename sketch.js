f6fa2e9be01bf9840814b3264ab3136dc0c45a8f
bea0033 <00001089200869sp@al.educacao.sp.gov.br>
2024-08-27 11:18:45 -0300

    Criando meu site

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 600);
+    background(238,130,238);
+  }
+  
+  function draw() {
+    fill(255,228,225)
+    circle(mouseX,mouseY,50)                                           
+  }
+  function mousePressed(){
+    background(238,130,238);
+    
+  }
